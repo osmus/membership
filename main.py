bb63d774e9d52f2aa11c95a99da6bf542a2ea7df
Ian Dees <ian.dees@gmail.com>
2016-10-30 19:05:48 -0400

    Add stderr logging in production mode

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import os
 import pprint
 import stripe
+import logging
 
 SECRET_KEY = 'development key'
 DEBUG = True
@@ -26,6 +27,15 @@ app.config.from_object(__name__)
 github = GitHub(app)
 
 stripe.api_key = app.config.get('STRIPE_SECRET_KEY')
+
+
+@app.before_first_request
+def setup_logging():
+    if not app.debug:
+        # In production mode, add log handler to sys.stderr.
+        app.logger.addHandler(logging.StreamHandler())
+        app.logger.setLevel(logging.INFO)
+
 
 @github.access_token_getter
 def token_getter():
