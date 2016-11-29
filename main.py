9196f6223d4f87babbfe564869c111164f58c5a8
Ian Dees <ian.dees@gmail.com>
2016-11-29 16:02:37 -0500

    Add an index page

--- main.py
+++ main.py
@@ -539,5 +539,10 @@ def github_logout():
     return redirect(url_for('member_list'))
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
